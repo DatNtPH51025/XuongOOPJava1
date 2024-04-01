031d5afab3c03b1bb75d2b083e7ba9b0cb776cd4
DatNtPH51025 <datntph51025@gmail.com>
2024-04-01 21:38:20 +0700

    add file

---
+++ XuongOOPJava_MD19303_Nhom1/src/xuongoop_ph51025_md19303/CanBo.java
@@ -0,0 +1,59 @@
+package xuongoop_ph51025_md19303;
+
+public class CanBo {
+    // Thuộc tính
+    private int maNV;
+    private String hoTen;
+    private double thue;
+    private double luong;
+
+    // Constructor
+    public CanBo(int maNV, String hoTen, double thue, double luong) {
+        this.maNV = maNV;
+        this.hoTen = hoTen;
+        this.thue = thue;
+        this.luong = luong;
+    }
+
+    // Phương thức tính lương
+    public float Tinh_Thue() {
+    if (luong <= 9000000) {
+        return 0;
+    } else if (luong <= 15000000) {
+        return (float) (0.1 * (luong - 9000000));
+    } else {
+        return (float) (0.1 * (15000000 - 9000000) + 0.12 * (luong - 15000000));
+    }
+}
+
+    public float Tinh_Luong() {
+        return (float) (luong - Tinh_Thue());
+    }
+
+    // Phương thức nhập thông tin cán bộ
+    public void Nhap(int maNV, String hoTen, double thue, double luong) {
+        this.maNV = maNV;
+        this.hoTen = hoTen;
+        this.thue = thue;
+        this.luong = luong;
+    }
+
+    // Phương thức xuất thông tin cán bộ
+    public void Xuat() {
+        System.out.println("Mã số: " + maNV);
+        System.out.println("Họ và tên: " + hoTen);       
+        System.out.println("Lương : " + Tinh_Luong());
+    }
+
+    
+}
+
+
+    
+
+
+
+    
+    
+    
+
